82456b22751f649dcc349ac0fe5ec72396594bb7
Eddy Brown <edward.brown@yunojuno.com>
2022-04-27 20:38:18 +0100

    Add logging command comments

--- src/logging.ts
+++ src/logging.ts
@@ -1,14 +1,34 @@
 import { error, info } from "@actions/core";
 
+/**
+ * Prints an info message
+ *
+ * @param message log message
+ * @param app app name for log format
+ */
 export const printInfo = (message: string, app?: string) => {
   info(!!app ? formatAppMessage(message, app) : message);
 };
 
+/**
+ * Prints a green success message
+ *
+ * @param message log message
+ * @param app app name for log format
+ */
 export const printSuccess = (message: string, app?: string) => {
   const greenMessage = `\u001b[32m${message}`;
   info(!!app ? formatAppMessage(greenMessage, app) : greenMessage);
 };
 
+/**
+ * Prints a red error message
+ *
+ * Note: this doesn't actually throw an error
+ *
+ * @param message log message
+ * @param app app name for log format
+ */
 export const printError = (message: string, app?: string) => {
   const redMessage = `\u001b[31m${message}`;
   error(!!app ? formatAppMessage(redMessage, app) : redMessage);
